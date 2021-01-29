71ba660aaeeb7326369f70aff46482a4dd274141
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-29 15:54:40 -0500

    first

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get '/posts', to: 'posts#index'
+
+  get '/posts/:id', to: 'posts#show'
+
+  post '/posts', to: 'posts#create'
+
+  put '/posts/:id', to: 'posts#update'
+
+  delete '/posts/:id', to: 'posts#delete'
+
+  post '/postsseed', to: 'posts#create_many'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
