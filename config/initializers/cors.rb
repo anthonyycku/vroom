8a1b34a8fbca3e11b035c809244c764af0d6e662
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-30 15:10:55 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', '*'
-
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  allow do
+    origins 'http://localhost:3000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
